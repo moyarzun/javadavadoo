294297d649c195cb68a2cb164004ffbc0ce8292b
moyarzun <mauricio2141@gmail.com>
2014-09-01 10:53:31 -0400

    Menú en viewMain con botón de Salida y Acerca de...

--- javadavadoo/src/javadavadoo/viewMain.java
+++ javadavadoo/src/javadavadoo/viewMain.java
@@ -3,6 +3,11 @@ package javadavadoo;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 
 public class viewMain extends Shell {
 
@@ -32,6 +37,43 @@ public class viewMain extends Shell {
 	 */
 	public viewMain(Display display) {
 		super(display, SWT.SHELL_TRIM);
+		
+		Button btnAceptar = new Button(this, SWT.NONE);
+		btnAceptar.setTouchEnabled(true);
+		btnAceptar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnAceptar.setBounds(349, 207, 75, 25);
+		btnAceptar.setText("Aceptar");
+		
+		Menu menu = new Menu(this, SWT.BAR);
+		setMenuBar(menu);
+		
+		MenuItem mntmArchivo_1 = new MenuItem(menu, SWT.CASCADE);
+		mntmArchivo_1.setText("Archivo");
+		
+		Menu menu_1 = new Menu(mntmArchivo_1);
+		mntmArchivo_1.setMenu(menu_1);
+		
+		MenuItem mntmSalir = new MenuItem(menu_1, SWT.NONE);
+		mntmSalir.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				dispose();
+			}
+		});
+		mntmSalir.setText("Salir");
+		
+		MenuItem mntmAyuda = new MenuItem(menu, SWT.CASCADE);
+		mntmAyuda.setText("Ayuda");
+		
+		Menu menu_2 = new Menu(mntmAyuda);
+		mntmAyuda.setMenu(menu_2);
+		
+		MenuItem mntmAcercaDe = new MenuItem(menu_2, SWT.NONE);
+		mntmAcercaDe.setText("Acerca de...");
 		createContents();
 	}
 
@@ -39,7 +81,7 @@ public class viewMain extends Shell {
 	 * Create contents of the shell.
 	 */
 	protected void createContents() {
-		setText("SWT Application");
+		setText("Javadavadoo");
 		setSize(450, 300);
 
 	}
@@ -48,5 +90,4 @@ public class viewMain extends Shell {
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components
 	}
-
 }
